384c6f10aeceefd1e1e512f8e0cb9017a304b8fc
zakytaufiq <79309269+zakytaufiq@users.noreply.github.com>
2021-08-25 17:52:05 +0700

    Add files via upload

---
+++ src/custom.js
@@ -0,0 +1,79 @@
+var mainslider;
+var sliding = false;
+$(document).ready(function(){
+    var options = {
+        slides: '.slide', //Nama slide di slidecontainer
+        swipe: false , //handler swipe, wajib include touchSwipe
+        transition: 'fade', //Transisi slide +> slide dan fade
+        slideTracker: true, //menambah pelacakan slide
+        slideTrackerID: 'slideposition', //Nama pelacakan slide
+        slideOnInterval: false, //Interval slide
+        interval: 9000, //Interval slide, jika slideOnInterval is enabled/true
+        animateDuration: 1000, //Durasi animasi
+        animationEasing: 'ease', //Nilai yang diterima: linear ease ini-out snap easeOutCubic
+                                    //easeInOutCubic easeInCirc easeOutCirc easeInOutCirc easeInExpo easeOutExpo
+                                    //easeInOutExpo easeInQuad easeOutQuad easeInOutQuad easeInQuart easeOutQuart 
+                                    //easeInOutQuart easeInQuint easeOutQuint easeInOutQuint easeInSine easeOutSine 
+                                    //easeInOutSine easeInBack easeOutBack easeInOutBack
+        pauseOnHover: false, //Pause jika user mengarahkan kursor ke slide container
+        magneticSwipe: true, //efek menempel pada slide ketika swipping/dragging
+        neverEnding: true //aktifkan untuk membuat efek yang tidak pernah berhenti.
+        
+    };
+    $(".slider").simpleSlider(options);
+    mainslider = $(".slider").data("simpleslider");
+
+    // yes, that's all!
+
+    $(".slider").on("beforeSliding", function(event){
+        var prevSlide = event.prevSlide;
+        var newSlide = event.newSlide;
+        $(".slider .slide[data-index='" + prevSlide + "'] .slidecontent").fadeOut();
+        $(".slider .slide[data-index='" + newSlide + "'] .slidecontent").hide();
+        sliding = true;
+    });
+
+    $(".slider").on("afterSliding", function(event){
+        var prevSlide = event.prevSlide;
+        var newSlide = event.newSlide;
+        $(".slider .slide[data-index='" + newSlide + "'] .slidecontent").fadeIn();
+        sliding = false;
+    });
+
+    //Control the slider by scrolling
+
+    $(window).bind('mousewheel', function(event){
+        if(!sliding){
+            if (event.originalEvent.deltaY > 0) {
+                mainslider.nextSlide();
+            }
+            else {
+                mainslider.prevSlide();
+            }
+        }
+    });
+
+    // slide show the slider 
+    slideShow()
+    function slideShow() {
+        var i;
+        var slides = document.getElementsByClassName("slide");
+        for (i = 1; i < slides.length; i++) {
+            mainslider.nextSlide();
+        }
+        setTimeout(slideShow, 10000);
+    }
+
+    $(".slide#satu").backstretch("images/image1.jpg");
+    $(".slide#dua").backstretch("images/image2.jpg");
+    $(".slide#tiga").backstretch("images/image3.jpg");
+    $(".slide#empat").backstretch("images/image4.jpg");
+    $(".slide#lima").backstretch("images/image5.jpg");
+    $(".slide#enam").backstretch("images/image6.jpg");
+
+    $('.slide .backstrech img').on('dragstart', function(event) {event.preventDefault(); });
+
+    $(".slidecontent").each(function(){
+        $(this).css('margin-top', -$(this).height()/2);
+    });
+});
